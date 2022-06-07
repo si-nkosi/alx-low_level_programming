0a8616baf01d917d98f98cee6e9f5cb265b18d96
Sipho Nkosi <siphoinnocentnkosi@icloud.com>
2022-06-07 07:36:43 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
 	return (0);
 }
