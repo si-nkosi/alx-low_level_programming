5602c807981c72e0db17f13dc1be300655933784
Sipho Nkosi <siphoinnocentnkosi@icloud.com>
2022-06-07 08:06:57 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *
+ * main - void
+ * Description: Prints the alphabet
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
