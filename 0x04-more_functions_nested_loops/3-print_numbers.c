d123d7d5566e3cc2d39742d642b44e9f0fd9a56f
Sipho Nkosi <siphoinnocentnkosi@icloud.com>
2022-06-09 14:51:27 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: Numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n=0; n<=9; n++)
+	{
+		_putchar(n + '0');
+	}
+
+	_putchar('\n')
+}
